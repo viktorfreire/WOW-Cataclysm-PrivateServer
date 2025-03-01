99f5a0d3027e548806cad395086207aac8be7c0d
VIKTORFREIRE <freire.victormanuel@gmail.com>
2025-03-01 09:30:07 +0000

    Add files via upload

---
+++ testcata/index.php
@@ -0,0 +1,73 @@
+<?php
+	ob_start();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="Cache-Control" content ="no-cache">
+	<meta name="google-site-verification" content="n-RadkNV7jQoWajDl5Jis0x_YZfrpzNyVnfQ9-k2KNY" />
+	<meta name="verify-iw" content="IW1020463190060706" />
+	<meta name="distribution" content="Global">
+	<meta name="generator" content="Notepad" />
+	<meta name="DC.Language" content="es_ES" />
+	<meta name="DC.Publisher" content="INGENIERO VICTOR FREIRE - Contacto: +351918680251, freire.victormanuel@gmail.com" />
+	<meta name="DC.Creator" content="INGENIERO VICTOR FREIRE - Contacto: +351918680251, freire.victormanuel@gmail.com" />
+	<meta name="robots" content="index,follow"  />
+	<meta name="description" content="::: TESTCATA 4.2.2 :::" />
+	<META name="keywords" content="TESTCATA, WOW, Cataclysm, World of Warcraft, Victor Manuel Freire Parada, Darwin Frique, Valencia, Venezuela, Portugal, Aveiro, Games, Gamers, Juegos, On-line, Video Juegos">
+	<meta name="DC.Title" content="TESTCATA 4.2.2" />
+	<meta name="Copyright" content="2012-2015 Ing. Victor Manuel Freire Parada. Todos los Derechos Reservados.">
+	<link rel="SHORTCUT ICON" href="imagenes/jpg/mano001.jpg" type="image/x-icon" />
+    <title>TESTCATA 4.2.2</title>
+	<link href="css/estilos.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" language="javascript" src="libreria/js/jquery-latest.js"></script>
+    <script type="text/javascript" language="javascript" src="libreria/js/rounded_corners.inc.js"></script>
+    <script type="text/javascript" language="javascript" src="libreria/js/jquery.validate.js"></script>
+    <script type="text/javascript" language="javascript" src="libreria/js/funciones.js"></script>
+</head>
+ 
+<body>
+	<div id="art-page-background-glare">
+		<div id="art-page-background-glare-image"></div>
+	</div>
+	<div align="center">
+        <div id="cuerpo" class="cuerpo">
+            <div class="entrada" align="center">
+            	<div class="titulo">
+                	Testcata 
+                </div>
+                <div class="msg" id="msg">
+                   	
+                </div>
+                <br /><br /><br />
+                <div class="datos">
+                
+                </div>
+            </div>
+            <div align="center">
+                <div class="logo_vwd">
+                	<br />
+            	</div>
+            </div>
+        </div>
+	</div>
+</body>
+</html>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-37250194-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<script  language="javascript"  type="text/javascript">iw_ad_popup_abre_iw=1; iwsrcplus="http://codenew.impresionesweb.com/p/popup.php?idrotador=102046&iw_b_np=1&lgid="+((new Date()).getTime() % 2147483648) + Math.random(); document.write("<scr"+"ipt language=javascript  type=text/javascript src="+iwsrcplus+"></scr"+"ipt>");</script>
+<?php
+	@require('libreria/php/session.php');
+?>
